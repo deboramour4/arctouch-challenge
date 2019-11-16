8128c0daee2379ab7972d48c471e3835250d7327
Débora Moura <deboramour4@gmail.com>
2019-11-16 12:01:04 -0300

    Initial project base

---
+++ QuizArcTouch/QuizArcTouch/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  QuizArcTouch
+//
+//  Created by Débora Oliveira on 16/11/19.
+//  Copyright © 2019 Débora Oliveira. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+                
+        self.window?.rootViewController = navigationController
+        
+        return true
+    }
+
+}
+
---
+++ QuizArcTouch/QuizArcTouch/Views/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  QuizArcTouch
+//
+//  Created by Débora Oliveira on 16/11/19.
+//  Copyright © 2019 Débora Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
